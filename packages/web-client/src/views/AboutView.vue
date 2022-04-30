67168178e0d5f0af7a0e4f5af2dcf3003f46a239
jito <sergio.sergio.98@hotmail.com>
2022-04-30 12:17:03 -0400

    feat(frontend): initial environment to work in frontend
    add Vue environment using Vue CLI, also add bootstrap, sass and Axios

---
+++ packages/web-client/src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button type="button" class="btn btn-success">Bootstrap button</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    & > button {
+      margin-top: 20px
+    }
+  }
+}
+</style>
